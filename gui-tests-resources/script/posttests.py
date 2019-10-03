1a752abda3bc756afd6b1b4eeb8cb8ae649cca62
Mandarancio <manda.mgf@gmail.com>
2019-10-03 10:43:58 +0200

    added comments

--- gui-tests-resources/script/posttests.py
+++ gui-tests-resources/script/posttests.py
@@ -1,5 +1,5 @@
 """
-Simply extracts the list of json files in
+Clean the test directory after the tests are ended.
 
 @author: Martino Ferrari
 @email: manda.mgf@gmail.com
@@ -12,26 +12,23 @@ import shutil
 
 __BUILD_DIR__ = "testBuild"
 __PROPERTIES_FOLDER__ = "shared"
-__LIB_FOLDER__ = "lib"
-
-
-def clean_run(rootdir, basedir, testdir):
-    build_dir = os.path.join(rootdir, testdir, __BUILD_DIR__)
-    snap_dir = os.path.join(build_dir, basedir)
-    properties_dir = os.path.join(build_dir, __PROPERTIES_FOLDER__)
-    shutil.rmtree(snap_dir)
-    shutil.rmtree(properties_dir)
 
 
 if __name__ == "__main__":
+    # arg parser configuration
     parser = argparse.ArgumentParser()
     parser.add_argument("--basedir", help="SNAP GUI TEST base dir", default="snap")
     parser.add_argument("--rootdir", help="Root dir", default=".")
     parser.add_argument("--testdir", help="Test base dir", default="gui-tests-resources")
 
+    # parse arguments
     args = parser.parse_args()
-    rootdir = args.rootdir
-    testdir = args.testdir
-    basedir = args.basedir
+    rootdir = args.rootdir # project root path
+    testdir = args.testdir # test root path
+    basedir = args.basedir # snap qft test relative path 
 
-    clean_run(rootdir, basedir, testdir)
+    build_dir = os.path.join(rootdir, testdir, __BUILD_DIR__) # build path
+    snap_dir = os.path.join(build_dir, basedir) # copy of the snap tests in the build path 
+    properties_dir = os.path.join(build_dir, __PROPERTIES_FOLDER__) # copy of the properties in the build path 
+    shutil.rmtree(snap_dir) # remove snap test copy
+    shutil.rmtree(properties_dir) # remove properties copy
